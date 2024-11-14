6cf6b364b258f3b08ab5b47b6b3c3b5d94b188b9
dvan-hum <dvan-hum@student.42perpignan.fr>
2024-11-14 16:24:14 +0100

    Fix a leak

--- ft_lstget.c
+++ ft_lstget.c
@@ -6,7 +6,7 @@
 /*   By: dvan-hum <dvan-hum@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 11:23:23 by dvan-hum          #+#    #+#             */
-/*   Updated: 2024/11/08 11:33:42 by dvan-hum         ###   ########.fr       */
+/*   Updated: 2024/11/14 16:23:21 by dvan-hum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,15 +40,18 @@ void	ft_lstiter(t_list *lst, void (*f)(void *))
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*map;
+	void	*temp_content;
 	t_list	*temp_new;
 
 	map = NULL;
 	while (lst)
 	{
-		temp_new = ft_lstnew(f(lst->content));
+		temp_content = f(lst->content);
+		temp_new = ft_lstnew(temp_content);
 		if (!temp_new)
 		{
 			ft_lstclear(&map, del);
+			del(temp_content);
 			return (NULL);
 		}
 		ft_lstadd_back(&map, temp_new);
